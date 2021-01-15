bf2ce33bbec7d855ef4fc530b73d6141d2c1508c
Thomas Jager <thomasj@thomasjager.ca>
2021-01-15 08:18:29 -0500

    Use tagged releases

--- Formula/vasm-all.rb
+++ Formula/vasm-all.rb
@@ -1,8 +1,8 @@
 class VasmAll < Formula
   desc "vasm portable and retargetable assembler."
   homepage "http://sun.hasenbraten.de/vasm/"
-  url "http://sun.hasenbraten.de/vasm/release/vasm.tar.gz"
-  version "1.8f"
+  url "http://phoenix.owl.de/tags/vasm1_8h.tar.gz"
+  version "1.8h"
   sha256 "2b7aba9b6d0a196a2ab009fbed08f10acd94da41d11d3a224cb59b2a6c2f2b41"
   
   def install
