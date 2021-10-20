39bcd52279426e9b226e693438241964dd51f827
Ross Bamford <roscopeco@gmail.com>
2021-10-20 23:46:50 +0100

    First cut MAME SBC formula

---
+++ Formula/mame-sbc.rb
@@ -0,0 +1,15 @@
+class MameSBC < Formula
+  desc "MAME for for Single Board Computers"
+  homepage "https://github.com/mmicko/mame"
+  head "https://github.com/mmicko/mame.git"
+
+  depends_on "qt5"
+
+  def install
+    make -j9 sbc64 install 
+  end
+
+  test do
+    system "sh", "'sbc64 -version | grep mame'"
+  end
+end
