6d6739ed84c05f7335184e72e10a3571c89ba51d
Xark <Xark@XarkLabs.com>
2023-07-04 10:57:20 -0700

    Add texinfo dependency for homebrew

--- Formula/binutils-cross-m68k.rb
+++ Formula/binutils-cross-m68k.rb
@@ -4,6 +4,8 @@ class BinutilsCrossM68k < Formula
   url "http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/binutils/binutils-2.40.tar.bz2"
   sha256 "f8298eb153a4b37d112e945aa5cb2850040bcf26a3ea65b5a715c83afe05e48a"
 
+  depends_on "texinfo" => :build
+  
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
