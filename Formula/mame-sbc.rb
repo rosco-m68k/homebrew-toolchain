3bc2954acc200afd37e24e5794aba8aca9070063
Ross Bamford <roscopeco@gmail.com>
2024-01-01 11:33:04 +0000

    Build mame, not installing ROMs yet

--- Formula/mame-sbc.rb
+++ Formula/mame-sbc.rb
@@ -1,15 +1,18 @@
 class MameSbc < Formula
   desc "MAME for for Single Board Computers"
   homepage "https://github.com/roscopeco/mame"
-  head "https://github.com/roscopeco/mame.git"
+  head "https://github.com/roscopeco/mame.git", :branch => 'homebrew'
 
   depends_on "qt5"
+  depends_on "python"
 
   def install
-    system 'make', '-j9', 'sbc64', 'install' 
+    system 'make', '-j9'
+    # There is no make install for mame
+    bin.install "sbc"
   end
 
   test do
-    system "sh", "'sbc64 -version | grep mame'"
+    system "sh", "'sbc -version | grep mame'"
   end
 end
